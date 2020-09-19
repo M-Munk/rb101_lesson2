0e853c75d944c60906a1de1d2e3d316ec5a91b8c
Melanie Munk <mmunk1994@gmail.com>
2020-09-18 22:38:02 -0600

    Rubocop fixes

--- calculator/calculator_refactor.rb
+++ calculator/calculator_refactor.rb
@@ -19,7 +19,6 @@ def operation_to_message(op)
   end
 end
 
-
 prompt "Welcome to the Calculator! Enter your name: "
 
 name = ''
@@ -35,7 +34,6 @@ end
 
 prompt "Welcome #{name}"
 loop do
-
   number1 = ''
   loop do
     prompt "What's the first number?"
@@ -46,7 +44,6 @@ loop do
       prompt "Hmm... that doesn't look like a valid number?"
     end
   end
-  
   number2 = ''
   loop do
     prompt "What's the second number?"
@@ -57,10 +54,9 @@ loop do
       prompt "Hmm... that doesn't look like a valid number?"
     end
   end
-  
   operator = ''
   operator_prompt = <<-MSG
-    What operation would you like to perform?
+  What operation would you like to perform?
     1 - add
     2 - subtract 
     3 - multiply
@@ -77,21 +73,19 @@ loop do
       prompt "Must choose 1, 2, 3, or 4"
     end
   end
-  
   prompt "#{operation_to_message(operator)} the two numbers..."
-
   result = case operator
-  when '1'
-    number1.to_i + number2.to_i
-  when '2'
-    number1.to_i - number2.to_i
-  when '3'
-    number1.to_i * number2.to_i
-  when '4'
-    number1.to_i / number2.to_i
-  end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_i / number2.to_i
+           end
 
-  prompt"The result is #{result}"
+  prompt "The result is #{result}"
   prompt("Do you want to perform another calculation? Y to calculate again")
   answer = Kernel.gets.chomp
   break unless answer.downcase.start_with? 'y'
