cc449e55c428ce3cb7dab1e1572f5557c4b57315
Melanie Munk <mmunk1994@gmail.com>
2020-09-27 16:03:22 -0600

    refactored methods, corrected continue choices

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # README #
+Projects and problems as part of Launch School Ruby 101.
--- RPS/rock_paper_scissors.rb
+++ RPS/rock_paper_scissors.rb
@@ -14,14 +14,20 @@ scoreboard = {
   ties: 0
 }
 
+rounds_played = 5
+
 def prompt(message)
   Kernel.puts("==> #{message}")
 end
 
-def display_greeting
+def clr_scn
   system("clear") || system("cls")
+end
+
+def display_greeting(rounds)
+  clr_scn
   prompt("Welcome to Rock, Paper, Scissors, Spock, Lizard")
-  prompt("Unless you quit early, first player to 5 wins!")
+  prompt("Unless you quit early, first player to #{rounds} wins!")
   prompt("Good Luck!")
 end
 
@@ -68,8 +74,13 @@ end
 
 def play_again?
   prompt("Do you want to play again? (y/n)")
-  answer = gets.chomp
-  answer.downcase == 'y' || answer.downcase == 'yes' ? true : false
+  answer = ''
+  loop do
+    answer = gets.chomp.downcase
+    break if %w(n no y yes).include?(answer)
+    prompt("Invalid input! Please type 'y/yes' or 'n/no'.")
+  end
+  %w(y yes).include?(answer)
 end
 
 def update_score(score, human, computer, win_list)
@@ -82,23 +93,19 @@ def update_score(score, human, computer, win_list)
   end
 end
 
-def check_score(score)
-  if score[:player] == 5
-    prompt("You win this round!")
-  elsif score[:computer] == 5
-    prompt("The computer wins this round!")
-  end
-end
-
-def game_over?(score)
-  score[:player] == 5 || score[:computer] == 5
+def game_over?(score, rounds)
+  score[:player] == rounds || score[:computer] == rounds
 end
 
 def print_score(score)
-  prompt("The final score is:")
   prompt("Player - #{score[:player]}")
   prompt("Computer - #{score[:computer]}")
   prompt("# of Ties - #{score[:ties]}")
+end
+
+def determine_winner(score)
+  prompt("The final score is:")
+  print_score(score)
   if score[:player] > score[:computer]
     prompt("You win this round!")
   elsif score[:player] == score[:computer]
@@ -110,23 +117,21 @@ end
 
 def display_current_score(score)
   prompt("The current score is:")
-  prompt("Player - #{score[:player]}")
-  prompt("Computer - #{score[:computer]}")
-  prompt("# of Ties - #{score[:ties]}")
+  print_score(score)
 end
 
 # main program
 
-display_greeting
+display_greeting(rounds_played)
 loop do
   choice = get_player_choice
   computer_choice = VALID_CHOICES.sample
   display_choices(choice, computer_choice)
   display_results(choice, computer_choice)
   update_score(scoreboard, choice, computer_choice, WIN_CONDITIONS)
-  break if game_over?(scoreboard)
+  break if game_over?(scoreboard, rounds_played)
   break unless play_again?
-  system("clear") || system("cls")
+  clr_scn
   display_current_score(scoreboard)
 end
-print_score(scoreboard)
+determine_winner(scoreboard)
