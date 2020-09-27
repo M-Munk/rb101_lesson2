016892f40ddbbd6571cffeb0df086d4a83c5bc67
Melanie Munk <mmunk1994@gmail.com>
2020-09-26 19:51:53 -0600

    added spock, lizard. Refactored

--- RPS/rock_paper_scissors.rb
+++ RPS/rock_paper_scissors.rb
@@ -1,13 +1,43 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors spock lizard)
 
 def prompt(message)
   Kernel.puts("==> #{message}")
 end
 
+def greeting()
+  prompt("Welcome to Rock, Paper, Scissors, Spock, Lizard")
+  prompt("Good Luck!")
+end
+
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'scissors' && second == 'lizard') 
+end
+
+def get_player_choice()
+  player_choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
+    player_choice = Kernel.gets().chomp()
+    if VALID_CHOICES.include?(player_choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
+  player_choice
+end
+
+def display_choices(player, computer)
+  Kernel.puts("You chose #{player}, computer chose #{computer}")
 end
 
 def display_results(player, computer)
@@ -20,24 +50,20 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-    choice = Kernel.gets().chomp()
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice")
-    end
-  end
-  computer_choice = VALID_CHOICES.sample()
-
-  Kernel.puts("You chose #{choice}, computer chose #{computer_choice}")
-  display_results(choice, computer_choice)
+def play_again?()
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer.downcase.start_with?('y') ? true: false
 end
 
+# main program 
+
+loop do
+  greeting
+  choice = get_player_choice()
+  computer_choice = VALID_CHOICES.sample()
+  display_choices(choice, computer_choice)
+  display_results(choice, computer_choice)
+  break unless play_again?
+end
 prompt("Thank you for playing. Good bye!")
